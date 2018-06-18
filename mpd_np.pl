43d15526605efbffda75dc1c3069187f49dbd141
japh <m@japh.se>
2018-06-18 12:17:01 +0200

    add styling

--- mpd_np.pl
+++ mpd_np.pl
@@ -9,128 +9,14 @@ use App::Pimpd::Info qw(current);
 my $mpd_np = current();
 
 print << "EOF"
-Nu spelas: <b>$mpd_np</b> <br><br>
-<a href="http://192.168.10.100:9998/japh.mp3">stay awhile, and listen</a>
+<title>$mpd_np</title>
+<body style="background-color:#288582">
+<font color="#0d3938">
+<br><br>
+<center>Nu spelas: <b>$mpd_np</b></center> <br><br>
+<center><a href="http://192.168.10.100:9998/japh.mp3"><img src="./deckard.png"></a></center">
 <br>
 EOF
 
 
-
-
-
-
-__END__
-
-
-=pod
-
-=head1 NAME
-
-=head1 USAGE
-
-=head1 DESCRIPTION
-
-=head1 OPTIONS
-
-=head1 REPORTING BUGS
-
-Report bugs and/or feature requests on rt.cpan.org, the repository issue tracker
-or directly to L<m@japh.se>
-
-=head1 AUTHOR
-
-  Magnus Woldrich
-  CPAN ID: WOLDRICH
-  m@japh.se
-  http://japh.se
-  http://github.com/trapd00r
-
-=head1 CONTRIBUTORS
-
-None required yet.
-
-=head1 COPYRIGHT
-
-Copyright 2013 B<THIS APPLICATION>s L</AUTHOR> and L</CONTRIBUTORS> as listed
-above.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SEE ALSO
-
-L<~/|http://japh.se>
-
-=cut
-#!/usr/bin/perl
-# vim: ft=perl:fdm=marker:fmr=#<,#>:fen:et:sw=2:
-use strict;
-use warnings FATAL => 'all';
-use vars     qw($VERSION);
-use autodie  qw(:all),
-
-my $APP  = '';
-$VERSION = '0.001';
-
-use Devel::Comments;
-use Data::Dumper;
-
-{
-  package Data::Dumper;
-  no strict 'vars';
-  $Terse = $Indent = $Useqq = $Deparse = $Sortkeys = 1;
-  $Quotekeys = 0;
-}
-
-
-
-
-
-__END__
-
-
-=pod
-
-=head1 NAME
-
-=head1 USAGE
-
-=head1 DESCRIPTION
-
-=head1 OPTIONS
-
-=head1 REPORTING BUGS
-
-Report bugs and/or feature requests on rt.cpan.org, the repository issue tracker
-or directly to L<m@japh.se>
-
-=head1 AUTHOR
-
-  Magnus Woldrich
-  CPAN ID: WOLDRICH
-  m@japh.se
-  http://japh.se
-  http://github.com/trapd00r
-
-=head1 CONTRIBUTORS
-
-None required yet.
-
-=head1 COPYRIGHT
-
-Copyright 2013 B<THIS APPLICATION>s L</AUTHOR> and L</CONTRIBUTORS> as listed
-above.
-
-=head1 LICENSE
-
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 SEE ALSO
-
-L<~/|http://japh.se>
-
-=cut
 
